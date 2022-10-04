f155a8c032f81d6a28df53f33cf4dd70648eb87d
TsungHan <2hadolphin@gmail.com>
2022-10-04 13:09:55 +0800

    Remove redundant files.

--- Assets/Return/StateSystem/IDamageState.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public interface IDamageState
-{
-    DamageState.Result Sampling();
-    DamageState.Result Sampling(float gusetLuck);
-    void ReceiveDamage(DamageState.Package package);
-    void ReceiveDamage(DamageState.DamageType type, int Damage);
-}
